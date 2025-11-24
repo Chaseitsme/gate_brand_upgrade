311474d7ea9144df8d409a9ee85e82d4a3606cd8
Chaseitsme <chi0yuhuan@gmail.com>
2025-11-24 17:06:48 +0800

    2

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Gate品牌升级设计展示/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
     }
   }
-})
+}))
